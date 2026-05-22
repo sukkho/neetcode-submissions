24622845bcf47910fe26857ab5feb2905e74402d
Aiya Sukkho <aiyasukkho@gmail.com>
2026-05-22 09:48:30 +0800

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.py
@@ -0,0 +1,19 @@
+class Solution: #sort the array and use two pointers to find the solution
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        A = []
+        for i, num in enumerate(nums):
+            A.append([num,i]) #value and index
+        
+        A.sort()
+        i = 0 #point to beginning of array
+        j = len(nums)-1 #point to end of array
+
+        while i < j:
+            sum = A[i][0] + A[j][0]
+            if sum == target:
+                return [min(A[i][1], A[j][1]), max(A[i][1], A[j][1])] #at position 0, stores value, at position 1 stores index
+            elif sum < target:
+                i += 1
+            else:
+                j -= 1
+        return []
